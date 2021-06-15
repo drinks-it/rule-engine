ac63ab54a53426c91ad8d94bd309de654bca328f
maxim.gubar <maxim.gubar@drink.ch>
2021-06-15 14:53:33 +0200

    added github workflow

---
+++ tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\DrinksIt\Bundle\RuleEngine;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testFoo(): void
+    {
+        self::assertTrue(true);
+    }
+}
