8ad0ec50f8f0a52dc5f099cbfdc2446edbd7e669
Oleg Tytarenko <oleg@drinks.group>
2021-07-06 18:38:18 +0300

    GA: Check with api platform

---
+++ src/Serializer/SerializerConditionsFieldInterface.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of Rule Engine Symfony Bundle.
+ * © 2010-2021 DRINKS | Silverbogen AG
+ */
+
+declare(strict_types=1);
+
+namespace DrinksIt\RuleEngineBundle\Serializer;
+
+use DrinksIt\RuleEngineBundle\Rule\CollectionConditionInterface;
+
+interface SerializerConditionsFieldInterface
+{
+    public function decodeToConditionCollection(array $conditions): CollectionConditionInterface;
+
+    public function encodeToConditionArray(CollectionConditionInterface $collectionCondition): array;
+}
